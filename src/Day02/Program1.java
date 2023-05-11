1f3580fbda97cfe4f62684580bd93da623d2ec1f
akhila-manu <132345929+akhila-manu@users.noreply.github.com>
2023-05-11 11:15:08 +0530

    Initial commit

---
+++ src/Day02/Program1.java
@@ -0,0 +1,12 @@
+package Day02;
+
+public class Program1 {
+
+	public static void main(String[] args) {
+		System.out.println("Good day....!");
+		System.out.println("Good Morning....!");
+		
+
+	}
+
+}
---
+++ src/Day03/program2.java
@@ -0,0 +1,36 @@
+package Day03;
+
+public class program2 {
+
+	public static void main(String[] args) 
+	{
+		add(10,20);
+		mul(10,20);
+		div(1000,20);
+		sub(100,200);
+		add(10,2000);
+	}
+		
+		public static void add(int a,int b)
+		{
+		System.out.println(a+b);
+		}
+		public static void sub(int a,int b)
+		{
+		System.out.println(a-b);
+		}
+		public static void mul(int a,int b)
+		{
+		System.out.println(a*b);
+		}
+		
+		public static void div(int a,int b)
+		{
+		System.out.println(a/b);
+		}
+
+
+
+	}
+
+
